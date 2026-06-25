0d424bd014b6db6f712884608c6c0749b8b8b663
itsnobii <57056220+itsnobii@users.noreply.github.com>
2026-06-25 16:45:28 -0400

    Implement alternate playercam component

---
+++ src/app/components/combat/playercams-alt/playercams-alt.component.ts
@@ -0,0 +1,37 @@
+import { Component, computed, inject, OnInit } from "@angular/core";
+import { SafeResourceUrl } from "@angular/platform-browser";
+import { DataModelService } from "../../../services/dataModel.service";
+import { DisplayNameService } from "../../../services/displayName.service";
+import { PlayercamStreamService } from "../../../services/playercamStream.service";
+import { OneVersusOneService } from "../../../services/1v1.service";
+
+@Component({
+  selector: "app-playercams-alt",
+  imports: [],
+  templateUrl: "./playercams-alt.component.html",
+  styleUrl: "./playercams-alt.component.css",
+})
+export class PlayercamsAltComponent implements OnInit {
+  readonly dataModel = inject(DataModelService);
+  readonly streamService = inject(PlayercamStreamService);
+  readonly oneVsOneService = inject(OneVersusOneService);
+  getDisplayName = inject(DisplayNameService).getDisplayName;
+
+  isOneVersusOne = computed(() => this.oneVsOneService.isOneVersusOne());
+
+  enabledPlayers = computed(() => {
+    let toReturn = this.dataModel.playercamsInfo().enabledPlayers;
+    if (!toReturn || toReturn.length === 0) {
+      toReturn = [];
+    }
+    return toReturn;
+  });
+
+  ngOnInit() {
+    this.streamService.initializeFromEnabledPlayers(this.enabledPlayers());
+  }
+
+  getStream(playerFullName: string): SafeResourceUrl | undefined {
+    return this.streamService.getStream(playerFullName);
+  }
+}
